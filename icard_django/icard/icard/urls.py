2f5e922243502b4e63f5d7d2161a8200c2333c7c
Raul M <118123543+pillow3@users.noreply.github.com>
2024-03-22 22:42:29 +0000

    first commit

---
+++ icard_django/icard/icard/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="iCard - APIDoc",
+      default_version='v1',
+      description="Documentation for iCard API",
+      terms_of_service="https://www.raulcano.dev/",
+      contact=openapi.Contact(email="rawraul@outlook.com"),
+      license=openapi.License(name="Raul License"),
+   ),
+   public=True,
+#    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
