bf43840e5026938b0907120aaf14898882a46d6e
Raul M <118123543+pillow3@users.noreply.github.com>
2024-03-24 21:14:32 +0000

    django routes

--- icard_django/icard/users/api/router.py
+++ icard_django/icard/users/api/router.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from users.api.views import UserApiViewSet, UserView
 
 router_user = DefaultRouter()
+
+# Registramos la ruta de la API de usuarios
 router_user.register(prefix='users', viewset=UserApiViewSet, basename='users')
 
 urlpatterns = [
