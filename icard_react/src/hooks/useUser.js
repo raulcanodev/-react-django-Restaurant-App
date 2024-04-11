94d28bd7c4f22bda2c49249496cfd461adfd0ada
Raul M <118123543+pillow3@users.noreply.github.com>
2024-04-11 22:24:45 +0100

    Admin page

--- icard_react/src/hooks/useUser.js
+++ icard_react/src/hooks/useUser.js
@@ -1,6 +1,13 @@
-import { getMeApi } from "../api/user";
+import { getMeApi, getUserApi } from "../api/user";
+import { useState } from "react";
+import { useAuth } from ".";
 
 export function useUser() {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [users, setUsers] = useState(null);
+	const { auth } = useAuth();
+
 	const getME = async (token) => {
 		try {
 			const response = await getMeApi(token);
@@ -9,5 +16,18 @@ export function useUser() {
 			throw e;
 		}
 	};
-	return { getME };
+
+	const getUsers = async () => {
+		try {
+			setLoading(true);
+			const response = await getUserApi(auth.token);
+			setLoading(false);
+			setUsers(response);
+		} catch (error) {
+			setLoading(false);
+			setError(error);
+		}
+	};
+
+	return { loading, error, users, getME, getUsers };
 }
--- icard_react/src/pages/Admin/UsersAdmin.jsx
+++ icard_react/src/pages/Admin/UsersAdmin.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+import { useUser } from "../../hooks";
+
 export function UsersAdmin() {
+	const { loading, users, getUsers } = useUser();
+	// console.log(loading);
+	console.log(users);
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
 	return <div>Users Admin</div>;
 }
