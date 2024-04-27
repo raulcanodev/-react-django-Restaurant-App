5f12a89dba8a470ddf03d02c586b92c9d5a9f565
Raul M <118123543+pillow3@users.noreply.github.com>
2024-04-27 17:30:18 +0100

    preview images categories

--- icard_react/src/components/Admin/Category/AddEditCategoryForm/AddEditCategoryForm.jsx
+++ icard_react/src/components/Admin/Category/AddEditCategoryForm/AddEditCategoryForm.jsx
@@ -1,14 +1,33 @@
 import "./AddEditCategoryForm.scss";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Image } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
+import { useState, useCallback } from "react";
 
 export function AddEditCategoryForm() {
+	const [previewImage, setPreviewImage] = useState(null);
+	const onDrop = useCallback((acceptedFiles) => {
+		const file = acceptedFiles[0];
+		setPreviewImage(URL.createObjectURL(file));
+		console.log("file", file);
+		console.log("url", URL.createObjectURL(file));
+	}, []);
+
+	const { getRootProps, getInputProps } = useDropzone({
+		accept: "image/jpeg, image/png",
+		multiple: false,
+		noKeyboard: true,
+		onDrop,
+	});
+
 	return (
 		<>
 			<Form className="add-edit-category-form">
 				<Form.Input name="title" placeholder="Category name" />
-				<Button type="button" fluid>
+				<Button type="button" fluid {...getRootProps()}>
 					Upload image
 				</Button>
+				<input {...getInputProps()} />
+				<Image src={previewImage} fluid />
 				<Button type="submit" content="Create" primary fluid></Button>
 			</Form>
 		</>
